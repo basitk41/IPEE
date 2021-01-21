f83974e552d7450b5db39b66307d673bcf9246e6
BASIT ALI <basitk41@gmail.com>
2021-01-22 02:49:51 +0500

    ok
    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Selecao - v2.3.0
-* Template URL: https://bootstrapmade.com/selecao-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
   "use strict";
 
